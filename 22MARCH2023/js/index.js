1ef77a53c2e90e9eddf55390972730ac7927f372
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-03-22 17:32:12 +0530

    practice

---
+++ 22MARCH2023/js/index.js
@@ -0,0 +1,45 @@
+// console.log("0987654321");
+
+// let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function test(data) {
+//     console.log(data);
+// }
+// test([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// function test(){
+//     // return 1
+// }
+// console.log(test()); 
+// console.log(test1());
+
+// function x(y){
+//     console.log("x");
+//     y()
+// }
+// x(function y(){
+//     console.log("y");
+// })
+
+
+// async function test(){
+    //  await 1
+    // }
+    // console.log(test())
+    // test().then((data)=>{console.log(data)})
+    // console.log("Welcome to Programiz!");
+    // async function test(){
+    //     let p1 = await test2()
+    //     console.log(p1);
+    // }
+    
+    // function test2(){
+    //     return new Promise((resolve,reject)=>{
+    //         setTimeout(function(){
+    //             console.log("hello")
+    //             resolve(1)
+    //         },1000)
+    //     })
+    // }
+    // test()
+
+
