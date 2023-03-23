fbe7fa2a10c965aa9bbdb94df8cadc6b68e89c40
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-03-23 17:31:44 +0530

    practice

---
+++ 23MARCH2023/js/index.js
@@ -0,0 +1,11 @@
+// console.log("0987654321");
+
+
+// async function test(){
+//     setTimeout(function (){
+//         console.log("timer");
+//     },5000);
+//     console.log("X");
+// }
+// test()
+
