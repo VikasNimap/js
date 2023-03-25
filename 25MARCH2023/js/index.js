690df1b86b74930530aeb5b619bcf4608cb62f06
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-03-25 17:42:47 +0530

    practice

---
+++ 25MARCH2023/js/index.js
@@ -0,0 +1,24 @@
+// console.log("qwerty");
+
+// let data = [12, 34, 5, 67, 23, 12];
+// let arr = [];
+// for (let i = 0; i < data.length; i++) {
+//     for (let j = i + 1; j < data.length; j++) {
+//         if (!arr.includes(data[i]) && data[i] == data[j]) {
+//             arr.push(data[i])
+//         }
+//     }
+// }
+// console.log(arr);
+
+// let data = ["vikas", false, 0, 25, undefined, null, true];
+// let filter = data.filter((d) => {
+//     return !d
+// })
+// console.log(filter);
+// let data = ["vikas", false, 0, 25, undefined, null, true];
+// let filter = data.forEach((d) => {
+//     if (!d) { console.log(d) }
+// })
+// console.log(filter);
+
