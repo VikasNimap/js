163357ad88b8ed96bfd681ef6a59f374d1999dfb
VikasNimap <vikasmaurya@nimapinfotech.com>
2023-03-29 17:28:03 +0530

    practice

---
+++ 29MARCH2023/js/index.js
@@ -0,0 +1,15 @@
+// console.log("12");
+
+// let data = [12, 34, 56, 78, 90, 90];
+// let arr = [];
+// for (let i = 0; i < data.length; i++) {
+//     for (let j = i + 1; j < data.length; j++) {
+
+//         if (!(arr.includes(data[i])) && data[i] == data[j]) {
+//             arr.push(data[i])
+//         }
+//     }
+// }
+// console.log(arr);
+
+
